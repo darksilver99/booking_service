4d4aa5c4ef7e518873cc89e9574d2ea7052a9a98
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-01-31 03:52:12 +0000

    Updating to latest FlutterFlow output.

--- lib/index.dart
+++ lib/index.dart
@@ -1,7 +1,7 @@
 // Export pages
 export '/home_page/home_page_widget.dart' show HomePageWidget;
-export '/login_page/login_page_widget.dart' show LoginPageWidget;
-export '/register_more_detail_page/register_more_detail_page_widget.dart'
+export '/member/login_page/login_page_widget.dart' show LoginPageWidget;
+export '/member/register_more_detail_page/register_more_detail_page_widget.dart'
     show RegisterMoreDetailPageWidget;
 export '/member/register_page/register_page_widget.dart'
     show RegisterPageWidget;
--- lib/member/register_more_detail_page/register_more_detail_page_model.dart
+++ lib/member/register_more_detail_page/register_more_detail_page_model.dart
@@ -3,6 +3,7 @@ import '/backend/backend.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
+import '/custom_code/actions/index.dart' as actions;
 import 'register_more_detail_page_widget.dart'
     show RegisterMoreDetailPageWidget;
 import 'package:cloud_firestore/cloud_firestore.dart';
