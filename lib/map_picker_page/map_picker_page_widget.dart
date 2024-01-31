8c46bd8bcaaaeac6f717a6d80bfd6d07234a7cc7
Darksilver88 <anusorn@koder3.com>
2024-01-31 14:43:04 +0700

    a

--- lib/map_picker_page/map_picker_page_widget.dart
+++ lib/map_picker_page/map_picker_page_widget.dart
@@ -1,3 +1,6 @@
+import 'package:geocoding/geocoding.dart';
+import 'package:map_picker/map_picker.dart';
+
 import '/flutter_flow/flutter_flow_google_map.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
@@ -21,6 +24,10 @@ class _MapPickerPageWidgetState extends State<MapPickerPageWidget> {
   late MapPickerPageModel _model;
 
   final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  final _controller = Completer<GoogleMapController>();
+  MapPickerController mapPickerController = MapPickerController();
+  CameraPosition? cameraPosition;
 
   @override
   void initState() {
@@ -60,23 +67,49 @@ class _MapPickerPageWidgetState extends State<MapPickerPageWidget> {
           top: true,
           child: Stack(
             children: [
-              FlutterFlowGoogleMap(
-                controller: _model.googleMapsController,
-                onCameraIdle: (latLng) => _model.googleMapsCenter = latLng,
-                initialLocation: _model.googleMapsCenter ??=
-                    LatLng(13.106061, -59.613158),
-                markerColor: GoogleMarkerColor.violet,
-                mapType: MapType.normal,
-                style: GoogleMapStyle.standard,
-                initialZoom: 14.0,
-                allowInteraction: true,
-                allowZoom: true,
-                showZoomControls: true,
-                showLocation: true,
-                showCompass: false,
-                showMapToolbar: false,
-                showTraffic: false,
-                centerMapOnMarkerTap: true,
+              MapPicker(
+                // pass icon widget
+                iconWidget: Icon(
+                  Icons.location_on,
+                  color: Colors.redAccent,
+                  size: 48,
+                ),
+                //add map picker controller
+                mapPickerController: mapPickerController,
+                child: GoogleMap(
+                  myLocationEnabled: true,
+                  zoomControlsEnabled: false,
+                  // hide location button
+                  myLocationButtonEnabled: false,
+                  mapType: MapType.normal,
+                  //  camera position
+                  initialCameraPosition: cameraPosition!,
+                  onMapCreated: (GoogleMapController controller) {
+                    _controller.complete(controller);
+                  },
+                  onCameraMoveStarted: () {
+                    // notify map is moving
+                    mapPickerController.mapMoving!();
+                  },
+                  onCameraMove: (cameraPosition) {
+                    this.cameraPosition = cameraPosition;
+                    // currentCenterPosition = LatLng(cameraPosition.target.latitude, cameraPosition.target.longitude);
+                  },
+                  onCameraIdle: () async {
+                    // notify map stopped moving
+                    mapPickerController.mapFinishedMoving!();
+                    //get address name from camera position
+                    List<Placemark> placemarks = await placemarkFromCoordinates(
+                      cameraPosition!.target.latitude,
+                      cameraPosition!.target.longitude,
+                    );
+                    // currentCenterPosition = LatLng(cameraPosition.target.latitude, cameraPosition.target.longitude);
+                    // update the ui with the address
+                    /* textController.text =
+                    '${placemarks.first.name}, ${placemarks.first.administrativeArea}, ${placemarks.first.country}';*/
+                    print(placemarks.first);
+                  },
+                ),
               ),
               Align(
                 alignment: AlignmentDirectional(0.0, 1.0),
@@ -87,6 +120,7 @@ class _MapPickerPageWidgetState extends State<MapPickerPageWidget> {
                         EdgeInsetsDirectional.fromSTEB(16.0, 0.0, 16.0, 16.0),
                     child: FFButtonWidget(
                       onPressed: () async {
+                        FFAppState().currentLocation = LatLng(cameraPosition!.target.latitude, cameraPosition!.target.longitude);
                         context.safePop();
                       },
                       text: 'เลือกพิกัดนี้',
