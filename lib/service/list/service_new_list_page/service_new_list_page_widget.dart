919b015a7e11b4792538ea978157f58caa95c8ee
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-02-02 10:57:47 +0000

    Updating to latest FlutterFlow output.

--- lib/service/list/service_new_list_page/service_new_list_page_widget.dart
+++ lib/service/list/service_new_list_page/service_new_list_page_widget.dart
@@ -595,7 +595,6 @@ class _ServiceNewListPageWidgetState extends State<ServiceNewListPageWidget> {
                           0,
                           16.0,
                         ),
-                        shrinkWrap: true,
                         reverse: false,
                         scrollDirection: Axis.vertical,
                         separatorBuilder: (_, __) => SizedBox(height: 8.0),
--- lib/service/list/service_rating_list_page/service_rating_list_page_widget.dart
+++ lib/service/list/service_rating_list_page/service_rating_list_page_widget.dart
@@ -596,7 +596,6 @@ class _ServiceRatingListPageWidgetState
                           0,
                           16.0,
                         ),
-                        shrinkWrap: true,
                         reverse: false,
                         scrollDirection: Axis.vertical,
                         separatorBuilder: (_, __) => SizedBox(height: 8.0),
