4c1a5f31248036236fd11ee6be69392d3a5d9fb4
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-02-09 09:26:25 +0000

    Updating to latest FlutterFlow output.

--- lib/payment/payment_category_list_page/payment_category_list_page_model.dart
+++ lib/payment/payment_category_list_page/payment_category_list_page_model.dart
@@ -1,4 +1,3 @@
-import '/components/information_dialog_view_widget.dart';
 import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
@@ -13,7 +12,6 @@ import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
-import 'package:webviewx_plus/webviewx_plus.dart';
 
 class PaymentCategoryListPageModel
     extends FlutterFlowModel<PaymentCategoryListPageWidget> {
